ca674642e5812cc67e3bc3724749dece173888b2
Rahat-Kibria <rahatkibriawahee@gmail.com>
2023-06-03 16:42:21 +0600

    responsive for all devices

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,15 @@
 <section id="footer">
+    <hr>
     <p>Updated at <time datetime="2023-05-31 08:30">May 31, 2023</time></p>
     <div class="social-links">
         <a href="https://www.facebook.com/wahy100" target="_blank" title="Facebook"><i
-                class="fa-brands fa-facebook fa-2x"></i></a>
-        <a href="https://github.com/rahat-kibria" target="_blank" title="Github"><i
-                class="fa-brands fa-github fa-2x"></i></a>
+                class="fa-brands fa-facebook"></i></a>
+        <a href="https://github.com/rahat-kibria" target="_blank" title="Github"><i class="fa-brands fa-github"></i></a>
         <a href="https://bitbucket.org/rahat-kibria" target="_blank" title="Bitbucket"><i
-                class="fa-brands fa-bitbucket fa-2x"></i></a>
+                class="fa-brands fa-bitbucket"></i></a>
         <a href="https://twitter.com/Rahatkibria1" target="_blank" title="Twitter"><i
-                class="fa-brands fa-twitter fa-2x"></i></a>
+                class="fa-brands fa-twitter"></i></a>
         <a href="https://www.linkedin.com/in/rahat-kibria-wahy" target="_blank" title="LinkedIn"><i
-                class="fa-brands fa-linkedin fa-2x"></i></a>
+                class="fa-brands fa-linkedin"></i></a>
     </div>
 </section>
